0f30bb70bac797a278c0e3873e46bc4cf2849425
Bohdan Yevtukh <bevtuh@gmail.com>
2020-12-17 19:46:28 +0200

    C++ compatibility header fix

--- include/algorithm.h
+++ include/algorithm.h
@@ -1,6 +1,14 @@
 #ifndef ALGORITHM__69E73852_9220_4FD4_BB64_A5984AA162CB__INCLUDED_
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 double my_pow(double base, int iexp);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // ALGORITHM__69E73852_9220_4FD4_BB64_A5984AA162CB__INCLUDED_
 
